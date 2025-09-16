b7f72463aeccf2fd1d7fc8dcf9f996f2cacc7c4e
Matveev Stepan <mset321@gmail.com>
2025-09-16 12:42:37 +0300

    feat: add lexer

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+#include "FlexLexer.h"
+
+int yyFlexLexer::yywrap() { return 1; }
+
+
+int main(int argc, char** argv) try {
+    if (argc != 2) {
+        std::cout << "usage ./jada file.adb" << std::endl;
+        return 1;
+    }
+
+    std::ifstream ifs(argv[1]);
+    if (!ifs.is_open()) {
+        std::cout << "Can`t open file\n";
+        return 1; 
+    }
+    yyFlexLexer lexer(&ifs);
+    while (lexer.yylex());
+
+    return 0;
+} catch (...) {
+    std::cerr << "Unknown error\n";
+    return 1;
+}
