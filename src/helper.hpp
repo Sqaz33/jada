4de9995f8806d87a672391b18daa76e5fcba0f2f
Matveev Stepan <mset321@gmail.com>
2025-10-22 23:24:01 +0300

    refactor: lexer.l code

--- src/helper.hpp
+++ src/helper.hpp
@@ -7,9 +7,8 @@
 
 namespace helper {
     extern yy::parser::semantic_type* yylval;
-    extern std::vector<std::string> modules;
+    extern std::vector<std::string> moduleFileNames;
     extern std::vector<std::string> errs;
-    extern std::string curModule;
     extern bool hasErr;
     extern int lineNo;  
     extern int columnNo;
