245deb3538b4fbe5172ec0666bf367fc48fcc88c
Matveev Stepan <mset321@gmail.com>
2026-01-19 00:45:34 +0300

    feat: add one test case

---
+++ test_data/name_error.adb
@@ -0,0 +1,42 @@
+with pack; use pack;
+
+with ada.text_io;
+
+-- pack.ads:  
+--  package pack is 
+--    X: Integer := 1;
+--  end pack;
+
+procedure main is 
+   type x is record 
+      X: Integer;
+   end record;
+
+
+   package a is
+      procedure z;
+      function y return x;
+   end a;
+
+   package body a is
+      procedure z is
+         Y: Integer;
+      begin
+         Y := 1;
+      end z;
+
+      function y return x is 
+         l: X; -- если назвать перм x - ошибка
+      begin 
+         l.X := 1;
+         return l;
+      end y;
+   end a;
+
+begin
+
+   ada.text_io.put_line(Integer'Image(1));
+
+end main;
+
+-- 
